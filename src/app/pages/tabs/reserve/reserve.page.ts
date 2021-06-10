3b95c96988029f4d06fda8fb03bfb3de737d0f00
Jonathan Marquez <jonicv_6@hotmail.com>
2021-06-11 01:10:09 +0200

    Refresh List Downmove and Delete Reserve

--- src/app/pages/tabs/reserve/reserve.page.ts
+++ src/app/pages/tabs/reserve/reserve.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ReserveDataService } from 'src/app/services/reserve-data.service';
 import { environment } from 'src/environments/environment';
 import { ReserveLocal } from 'src/models/reserveLocal';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reserve',
@@ -11,17 +13,59 @@ export class ReservePage {
 
   listReserves: ReserveLocal[] = [];
   env = environment;
-  
-  constructor() {
-    
 
-
-
+  constructor(
+    private reserveDataService: ReserveDataService) {
   }
 
   ngOnInit() {
-    this.listReserves = JSON.parse(localStorage.getItem('reserves'));
-    console.log(this.listReserves)
+    this.getDataLocal();
+  }
+
+  deleteReserve(reserve) {
+    Swal.fire({
+      title: this.env.titleDeleteReserve,
+      text: this.env.textDeleteReserve,
+      icon: 'warning',
+      heightAuto: false,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: this.env.buttonConfirmDelete,
+      cancelButtonText: this.env.buttonCancelDelete
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (this.listReserves.includes(reserve)) {
+          //Borramos la reserva de la lista
+          let index = this.listReserves.indexOf(reserve);
+          this.listReserves.splice(index, 1);
+          //Actualizamos la lista en local
+          localStorage.setItem('reserves', JSON.stringify(this.listReserves));
+          //Borramos la reserva de la base de datos
+          this.reserveDataService.deleteReserve(reserve).subscribe(r => {
+            console.log(r);
+            Swal.fire({
+              title: this.env.titleDeleted,
+              text: this.env.successReserveDeleted,
+              icon: 'success',
+              heightAuto: false
+            })
+          });
+        }
+      }
+    })
+  }
+
+  doRefresh(event) {
+    this.getDataLocal();
+    //Cuando finalice la lectura de datos, cancelamos el refresh.
+    event.target.complete();
+
+  }
+
+  //metodo para leer los datos locales
+  async getDataLocal() {
+    this.listReserves = await JSON.parse(localStorage.getItem('reserves'));
   }
 
 
