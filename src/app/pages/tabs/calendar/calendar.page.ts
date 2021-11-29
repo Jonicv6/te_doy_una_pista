674b2ea462bea532b3cb5edbde77e821c64b0fce
Jonathan Marquez <jonicv_6@hotmail.com>
2021-11-29 01:20:53 +0100

    Authentication

--- src/app/pages/tabs/calendar/calendar.page.ts
+++ src/app/pages/tabs/calendar/calendar.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/firebase/auth.service';
 
 @Component({
@@ -8,13 +9,37 @@ import { AuthService } from 'src/app/services/firebase/auth.service';
 })
 export class CalendarPage {
 
-  constructor(private googleAuth: AuthService) {}
+  constructor(private googleAuth: AuthService, private router: Router) {}
 
-  async onLogin() {
+  async onLogin(email, password) {
     try {
-      this.googleAuth.onLogin();
+      const user = await this.googleAuth.login(email.value, password.value);
+      if (user) {
+        const isVerified = this.googleAuth.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
     } catch (error) {
-      console.log(error)
+      console.log('Error->', error);
+    }
+  }
+
+  async onLoginGoogle() {
+    try {
+      const user = await this.googleAuth.loginGoogle();
+      if (user) {
+        const isVerified = this.googleAuth.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log('Error->', error);
+    }
+  }
+
+  private redirectUser(isVerified: boolean): void {
+    if (isVerified) {
+      this.router.navigate(['admin']);
+    } else {
+      this.router.navigate(['verify-email']);
     }
   }
 }
