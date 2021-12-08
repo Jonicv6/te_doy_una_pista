6a56c8e3b0ed6ecc7213a72fa98ea0a215d7f985
Jonathan Marquez <jonicv_6@hotmail.com>
2021-12-08 17:40:31 +0100

    Login Google

--- src/app/pages/tabs/calendar/calendar.page.ts
+++ src/app/pages/tabs/calendar/calendar.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+//import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { Platform } from '@ionic/angular';
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
@@ -12,7 +12,7 @@ import { GooglePlus } from '@ionic-native/google-plus/ngx';
 export class CalendarPage {
 
   constructor(
-    private afAuth: AngularFireAuth,
+    //private afAuth: AngularFireAuth,
     private platform: Platform,
     private googlePlus: GooglePlus
   ) {}
@@ -34,20 +34,20 @@ export class CalendarPage {
       'webClientId': "702844795474-sbh3uc19otjoi43n1esknkgmq431l1j5.apps.googleusercontent.com",
       'offline': true
     });
-    const resConfirmed = await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken));
+    /*const resConfirmed = await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken));
     const user = resConfirmed.user;
     this.picture = user.photoURL;
     this.name = user.displayName;
-    this.email = user.email;
+    this.email = user.email;*/
   }
 
   async loginGoogleWeb() {
-    const res = await this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    /*const res = await this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
     const user = res.user;
     console.log(user);
     this.picture = user.photoURL;
     this.name = user.displayName;
-    this.email = user.email;
+    this.email = user.email;*/
   }
   
 
