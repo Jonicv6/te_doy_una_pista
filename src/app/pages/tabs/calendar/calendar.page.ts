db337054c07cd0d95ceae5dd388e5caa0c26be28
Jonathan Marquez <jonicv_6@hotmail.com>
2021-12-08 18:41:05 +0100

    Eliminacion GooglePlus

--- src/app/pages/tabs/calendar/calendar.page.ts
+++ src/app/pages/tabs/calendar/calendar.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 //import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { Platform } from '@ionic/angular';
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 @Component({
   selector: 'app-calendar',
@@ -13,42 +12,17 @@ export class CalendarPage {
 
   constructor(
     //private afAuth: AngularFireAuth,
-    private platform: Platform,
-    private googlePlus: GooglePlus
-  ) {}
+    private platform: Platform  ) {}
 
   picture:string;
   name:string;
   email:string;
 
   loginGoogle() {
-    if (this.platform.is('android')) {
-      this.loginGoogleAndroid();
-    } else {
-      this.loginGoogleWeb();
-    }
+   
   }
 
-  async loginGoogleAndroid() {
-    const res = await this.googlePlus.login({
-      'webClientId': "702844795474-sbh3uc19otjoi43n1esknkgmq431l1j5.apps.googleusercontent.com",
-      'offline': true
-    });
-    /*const resConfirmed = await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken));
-    const user = resConfirmed.user;
-    this.picture = user.photoURL;
-    this.name = user.displayName;
-    this.email = user.email;*/
-  }
-
-  async loginGoogleWeb() {
-    /*const res = await this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    const user = res.user;
-    console.log(user);
-    this.picture = user.photoURL;
-    this.name = user.displayName;
-    this.email = user.email;*/
-  }
+  
   
 
 }
