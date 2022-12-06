240feaedc853429be2abff42f2e4ed33fa3cf18d
Jonathan Marquez <jonicv_6@hotmail.com>
2022-12-06 03:28:27 +0100

    Carga valores del usuario en el primer Init page

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -29,70 +29,67 @@ export class SearchPage {
     private sportCenterDataService: SportCenterDataService,
     private loadingCtrl: LoadingController,
     private trackDataService: TrackDataService) {
-   
+
   }
 
-  ngOnInit() { 
-     this.getData();
+  ngOnInit() {
+    console.log("INIT");
+    this.getData();
+    this.localData();
+
   }
 
-   async getData() {
-     //Activamos el loading y cargamos los datos
+  async getData() {
+    
+    console.log("GETDATA");
+    //Activamos el loading y cargamos los datos
     await this.presentLoading(environment.textLoading);
     this.loading.present();
-    
-    //Extraemos las ciudad de los SportCenters
-    await this.sportCenterDataService.getSportCenters()
-    .toPromise().then(result => {
-        //Recorremos los datos 
-        for (let i of result) {
-          //Si la ciudad está repetida, se descarta
-          if (!this.citys.includes(i.city)) {
-            this.citys.push(i.city);
-            //console.log(i.city);
-          }
-        };
-        this.city = this.citys[0];
-      }).catch((e) => {
-        console.log("TERMINADO ERROR: "+e);
-      });;
 
-    //Leemos los deportes que existen
-    await this.trackDataService.getTracks()
-      .toPromise().then(result => {
-        for (let i of result) {
-          let auxiliar = i.sport.split("-");
-          auxiliar.forEach(element => {
-            if (!this.sports.includes(element)) {
-              this.sports.push(element);
+      //Extraemos las ciudad de los SportCenters
+      await this.sportCenterDataService.getSportCenters()
+        .toPromise().then(result => {
+          //Recorremos los datos 
+          for (let i of result) {
+            //Si la ciudad está repetida, se descarta
+            if (!this.citys.includes(i.city)) {
+              this.citys.push(i.city);
+              //console.log(i.city);
             }
-          });
+          };
+          this.city = this.citys[0];
+        }).catch((e) => {
+          console.log("TERMINADO ERROR: " + e);
+        });;
 
-        }
-        this.sport = this.sports[0];
-        
-      });
-      //Paramos el loading
-      this.loading.dismiss();
-      
-      //Una vez cargado ambos, realiza la busqueda por primera vez
-      this.search(this.city, this.sport);
+      //Leemos los deportes que existen
+      await this.trackDataService.getTracks()
+        .toPromise().then(result => {
+          for (let i of result) {
+            let auxiliar = i.sport.split("-");
+            auxiliar.forEach(element => {
+              if (!this.sports.includes(element)) {
+                this.sports.push(element);
+              }
+            });
 
-      
+          }
+          this.sport = this.sports[0];
+
+        });
+
   }
 
 
   /* ------------ Realizamos la búsqueda según los valores elegidos ----------- */
   async search(city, sport) {
+    
+    console.log("SEARCH");
     this.sportCenters = [];
     this.sportAux = sport; //Guardamos el valor de deportes
 
     //Cambiamos los valores de los boolean
     this.init = false;
-
-    //Ejecutamos el Loading
-    await this.presentLoading(environment.textLoading);
-    this.loading.present();
 
     //Buscamos los SportCenters según la ciudad y el deporte indicado
     await this.sportCenterDataService.getSportCentersCityAndSport(city, sport)
@@ -104,8 +101,9 @@ export class SearchPage {
         } else {
           this.empty = false;
         }
-        //Paramos el loading
-        this.loading.dismiss();
+        
+      //Desactivamos el mensaje de carga
+      this.loading.dismiss();
       });
 
 
@@ -118,6 +116,22 @@ export class SearchPage {
       message,
       duration: 10000
     });
+  }
+
+  async localData() {
+    
+    console.log("LOCALDATA");
+    setTimeout(async () => {
+      //Busca en los archivos locales Profile, donde guardaremos los datos favoritos del usuario
+      if (localStorage.getItem('profile') != null) {
+        let dataLocal = await JSON.parse(localStorage.getItem('profile'));
+        this.sport = dataLocal['sport'];
+        this.city = dataLocal['city'];
+      }
+      //Una vez cargado ambos, realiza la busqueda por primera vez
+      this.search(this.city, this.sport);
+
+    }, 4000);
   }
 
 
