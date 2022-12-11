23f404976f2af8dbc3b93a3f731cfb7a529644cc
Jonathan Marquez <jonicv_6@hotmail.com>
2022-12-11 18:12:12 +0100

    Servicio Comment creado

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Comment } from 'src/models/comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private _comment: Comment;
+
+  constructor(private http: HttpClient) { }
+
+  httpHeader =  new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+
+  private setComment(value: Comment) {
+    this._comment = value;
+  }
+
+  public getComments(): Observable<any> {
+    return this.http.get(environment.endPoint + environment.commentPoint);
+  }
+
+  public createComment(comment:Comment) {
+    return this.http.post(environment.endPoint + environment.commentPoint, comment, { headers: this.httpHeader});
+  }
+
+}
