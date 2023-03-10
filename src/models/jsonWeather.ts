25094fae79bda9c41ff3dd7d2bb76d3df8340599
Jonathan Marquez <jonicv_6@hotmail.com>
2023-03-10 17:24:46 +0100

    Creacion Interfaz de Clima

---
+++ src/models/jsonWeather.ts
@@ -0,0 +1,12 @@
+
+export interface WeatherAPI {
+    latitude: number;
+    longitude: number;
+    timezone: string;
+    temperature_hour: number;
+    precipitation_hour: number;
+    weathercode_hour: number;
+    temperature_max: number;
+    temperature_min: number;
+    windspeed_max: number;          
+}
