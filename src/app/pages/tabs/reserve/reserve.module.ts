060627e575ca7e2e4156de828aaa2c58edd90c52
Jonathan Marquez <jonicv_6@hotmail.com>
2023-05-25 13:25:43 +0200

    Eliminacion ExplorerContainer

--- src/app/pages/tabs/reserve/reserve.module.ts
+++ src/app/pages/tabs/reserve/reserve.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReservePage } from './reserve.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { ComponentsModule } from 'src/app/components/components.module';
 import {MatTabsModule} from '@angular/material/tabs';
 import {ScrollingModule} from '@angular/cdk/scrolling';
@@ -18,7 +17,6 @@ import { ModalComment } from './modals/modalComment';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     ComponentsModule,
     MatTabsModule,
     ScrollingModule,
--- src/app/pages/tabs/search/search.module.ts
+++ src/app/pages/tabs/search/search.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SearchPage } from './search.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { ComponentsModule } from 'src/app/components/components.module';
 
@@ -13,7 +12,6 @@ import { ComponentsModule } from 'src/app/components/components.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     ComponentsModule,
     RouterModule.forChild([{ path: '', component: SearchPage }])
   ],
